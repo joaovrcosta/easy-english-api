04a0bc23bef0ab933bee2385a6954a9c6f5286a7
João Victor <joaovr.costa@gmail.com>
2024-07-29 10:54:54 -0300

    feat: added new dependencies

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import fastify from 'fastify'
+import { ZodError } from 'zod'
+import { env } from './env'
+import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
+
+export const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+})
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+})
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'validation error', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+  }
+
+  return reply.status(500).send({ message: 'internal server error' })
+})
---
+++ tsup.config.js
@@ -0,0 +1,1 @@
+export default { external: ['vitest', 'supertest'] }
