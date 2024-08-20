176ca9bfaf93b167e49dc2b5fa7d5ad723e9122e
João Victor <joaovr.costa@gmail.com>
2024-08-20 11:11:55 -0300

    feat: ensure list all students

--- src/repositories/students-repository.ts
+++ src/repositories/students-repository.ts
@@ -2,8 +2,5 @@ import { Student, Prisma, Address } from '@prisma/client'
 
 export interface StudentRepository {
   create(studentData: Prisma.StudentUncheckedCreateInput): Promise<Student>
-  // createWithAddress(
-  //   studentData: Prisma.StudentUncheckedCreateInput,
-  //   addressData?: Prisma.AddressUncheckedCreateInput
-  // ): Promise<{ student: Student; address?: Address }>
+  listAll(): Promise<Student[]>
 }
